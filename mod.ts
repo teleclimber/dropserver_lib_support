3de49dacadc49791de16909d686e0aed3c0e3fb2
O F <teleclimber@gmail.com>
2022-10-13 11:13:40 -0700

    feat: add handlerName to SandboxRoute
    - makes it psosible for dropserver_app to wrap handler and pass the name

--- mod.ts
+++ mod.ts
@@ -2,7 +2,8 @@
 // There is no need to use them unless you are writing an alternative 
 // to the dropserver_app library.
 
-// migrations:
+// Migrations:
+// App code creates migrations.
 export type migrationFunction = () => Promise<void>;
 
 export type Migration = {
@@ -17,7 +18,8 @@ interface migrations {
 	setCallback(cb:GetMigrationsCallback) :void
 }
 
-// user stuff:
+// Users:
+
 export type User = {
 	proxyId: string,
 	displayName: string,
@@ -29,7 +31,8 @@ interface users {
 	getAll() :Promise<User[]>
 }
 
-// Routes stuff:
+// App Routes:
+// App code creates app routes
 
 export interface Context {
 	req: Deno.RequestEvent
@@ -67,7 +70,8 @@ interface RouteBase {
 }
 export interface SandboxRoute extends RouteBase {
 	type: RouteType.function,
-	handler: Handler
+	handler: Handler,
+	handlerName: string
 }
 
 export type StaticOpts = {
@@ -87,6 +91,8 @@ interface AppRoutesIface {
 	setCallback(cb:GetAppRoutesCallback) :void
 }
 
+// libSupport interface is implemented by sandbox runner
+// and used by app code to connect the app code to the runner.
 export default interface libSupport {
 	appPath:string,
 	appspacePath:string,
